fae28e7003e7e7aa1102da9bc37fd333146460cf
JarrenWhite <99211534+JarrenWhite@users.noreply.github.com>
2023-06-24 11:11:59 +0100

    FrequencyForm.jsx completed to add values from dropdowns

--- time_cost/src/wage_form/FrequencyForm.jsx
+++ time_cost/src/wage_form/FrequencyForm.jsx
@@ -1,8 +1,19 @@
 function FrequencyForm({frequency, changeFrequency}){
+    const handleOptionChange = (event) => {
+        changeFrequency(event.target.value);
+    };
+
     return(
-        <label>
-            FrequencyForm
-        </label>
+        <div>
+            <select value={frequency} onChange={handleOptionChange}>
+                <option value="Hour">Hour</option>
+                <option value="Day">Day</option>
+                <option value="Week">Week</option>
+                <option value="Month">Month</option>
+                <option value="Year">Year</option>
+                <option value="Four Weeks">Four Weeks</option>
+            </select>
+        </div>
     )
 }
 
