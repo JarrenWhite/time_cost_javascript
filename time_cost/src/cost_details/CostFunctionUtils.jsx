81e4ea0ce63a9ae7dd8c3c12d7af8631a858930a
JarrenWhite <phillrucci@hotmail.co.uk>
2023-07-07 21:35:28 +0100

    (#11) Input form can read either decimal or string-like hour inputs. Both are interpreted correctly. However, hours pulled from cookies are no longer displayed on launch.

--- time_cost/src/cost_details/CostFunctionUtils.jsx
+++ time_cost/src/cost_details/CostFunctionUtils.jsx
@@ -2,34 +2,6 @@ export function CostFunctionUtils({salary, payFrequency, hours, hourFrequency}) 
     // Number of times per year that the event occurs
     const timesPerYear = {'Week': 52, 'Month': 12, 'Year': 1, 'Four Weeks': 13}
 
-    // Switch hours from human-readable format to decimal form
-    const hoursToDecimal = (readableHours) => {
-        // If no ':', return hours
-        if (!readableHours.includes(':')) {
-            return parseFloat(readableHours);
-        }
-
-        // If ends with ':', remove and return hours
-        if (readableHours.endsWith(':')) {
-            return parseFloat(readableHours.split(':')[0]);
-        }
-
-        // If contains both hours and minutes, split & add as decimals
-        const fullHours = parseFloat(readableHours.split(':')[0]);
-
-
-        // If minutes is one digit
-        const minutes = readableHours.split(':')[1];
-        if (minutes.length === 1) {
-            const numericMinutes = parseFloat(minutes) / 6
-            return fullHours + numericMinutes;
-        }
-
-        // Else minutes is two digits
-        const numericMinutes = parseFloat(minutes) / 60
-        return fullHours + numericMinutes;
-    }
-
     const recalculatePayPerHour = () => {
         // If payFrequency is hourly, already have pay per hour
         if (payFrequency === 'Hour') {
@@ -38,7 +10,8 @@ export function CostFunctionUtils({salary, payFrequency, hours, hourFrequency}) 
 
         // Else, calculate pay per year & hours per year
         const yearlySalary = parseFloat(salary) * timesPerYear[payFrequency];
-        const yearlyHours = hoursToDecimal(hours) * timesPerYear[hourFrequency];
+        const yearlyHours = parseFloat(hours) * timesPerYear[hourFrequency];
+        console.log('Hours: ' + hours);
         return (yearlySalary / yearlyHours);
     }
 
--- time_cost/src/wage_form/HoursForm.jsx
+++ time_cost/src/wage_form/HoursForm.jsx
@@ -1,24 +1,58 @@
 import {Form, InputGroup} from "react-bootstrap";
-import React from "react";
+import React, {useState} from "react";
 
 function HoursForm({hours, changeHours}) {
 
-    const hoursRegex = /^(\d+):?([0-5]\d?)?$/;
+    const hoursRegexDecimal = /^\d+(\.\d{0,2})?$/;
+    const hoursRegexString = /^(\d+):?([0-5]\d?)?$/;
 
+    // let displayedHours = hours;
+    const [displayedHours, setDisplayedHours] = useState(hours.toString());
+
+    // Switch hours from human-readable format to decimal form
+    const hoursToDecimal = (readableHours) => {
+        // If ends with ':', remove and return hours
+        if (readableHours.endsWith(':')) {
+            return readableHours.split(':')[0];
+        }
+
+        // If contains both hours and minutes, split & add as decimals
+        const fullHours = readableHours.split(':')[0];
+        const minutes = readableHours.split(':')[1];
+
+        // If minutes is one digit
+        if (minutes.length === 1) {
+            const numericMinutes = Math.round(parseInt(minutes) / 0.06);
+            return `${fullHours}.${numericMinutes.toString()}`;
+        }
+
+        // Else minutes is two digits
+        const numericMinutes = Math.round(parseInt(minutes) / 0.6);
+        return `${fullHours}.${numericMinutes.toString()}`;
+    }
+
+    // Handle user input
     const handleInputChange = (e) => {
-        const inputValue = e.target.value.replace('.', ':');
+        const inputValue = e.target.value;
 
-        if (inputValue === '' || hoursRegex.test(inputValue)) {
+        // If empty or decimal input, process as is
+        if (inputValue === '' || hoursRegexDecimal.test(inputValue)) {
             changeHours(inputValue);
+            setDisplayedHours(inputValue);
+        // If string style input, convert to decimal input
+        } else if (hoursRegexString.test(inputValue)) {
+            changeHours(hoursToDecimal(inputValue));
+            setDisplayedHours(inputValue);
         }
     }
+
 
     return(
         <Form.Group>
             <InputGroup>
                 <Form.Control
                     type="text"
-                    value={hours}
+                    value={displayedHours}
                     onChange={handleInputChange}
                     placeholder="Enter Hours Worked"
                 />
