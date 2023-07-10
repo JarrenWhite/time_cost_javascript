8c0fb755a417eaab93fd72ba3fe80f4e5c8b5188
JarrenWhite <phillrucci@hotmail.co.uk>
2023-07-10 22:37:42 +0100

    (#8) Flipped tax calculations to accumulate cost instead of net, to more easily integrate nino calculations

--- time_cost/src/cost_details/TaxCalcUtils.jsx
+++ time_cost/src/cost_details/TaxCalcUtils.jsx
@@ -6,25 +6,26 @@ const ninoAmounts = [0, 13.8]
 
 export function TaxCalcUtils() {
 
-    // Given an amount of money & a rate of tax, calculate amount kept
+    // Given an amount of money & a rate of tax, calculate amount payed
     const calculateTax = (amount, taxRate) => {
-        const keepRate = (100-taxRate) / 100;
-        return amount * keepRate;
+        const lossRate = taxRate / 100;
+        return amount * lossRate;
     }
 
     const calculateNetIncome = (grossIncome) => {
-        let netIncome = 0;
+        let taxCost = 0;
+        let ninoCost = 0;
         let i = 1;
 
         // Calculate net income from each bracket
         while (i < taxBrackets.length) {
             if (grossIncome <= taxBrackets[i]) {
                 const earnedInBracket = grossIncome - taxBrackets[i-1]
-                netIncome += calculateTax(earnedInBracket, taxAmounts[i-1]);
+                taxCost += calculateTax(earnedInBracket, taxAmounts[i-1]);
                 break;
             } else {
                 const bracketWidth = taxBrackets[i] - taxBrackets[i - 1];
-                netIncome += calculateTax(bracketWidth, taxAmounts[i-1]);
+                taxCost += calculateTax(bracketWidth, taxAmounts[i-1]);
             }
             i++;
         }
@@ -32,10 +33,11 @@ export function TaxCalcUtils() {
         // If income is greater than the highest bracket, add the difference
         if (grossIncome > taxBrackets[i-1]) {
             const earnedAtMax = grossIncome - taxBrackets[i-1]
-            netIncome += calculateTax(earnedAtMax, taxAmounts[i-1]);
+            taxCost += calculateTax(earnedAtMax, taxAmounts[i-1]);
         }
 
-        return netIncome;
+        const totalCost = taxCost + ninoCost;
+        return grossIncome - totalCost;
     }
 
     return {
