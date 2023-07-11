70a8a6a6df39e11aa851814b9e9757c0914bd1f2
JarrenWhite <phillrucci@hotmail.co.uk>
2023-07-11 18:46:08 +0100

    (#8) Amended tax cost function to reduce code duplication, whilst also calculating nino

--- time_cost/src/cost_details/TaxCalcUtils.jsx
+++ time_cost/src/cost_details/TaxCalcUtils.jsx
@@ -7,34 +7,40 @@ const ninoAmounts = [0, 13.8]
 export function TaxCalcUtils() {
 
     // Given an amount of money & a rate of tax, calculate amount payed
-    const calculateTax = (amount, taxRate) => {
+    const calculateCost = (amount, taxRate) => {
         const lossRate = taxRate / 100;
         return amount * lossRate;
     }
 
-    const calculateNetIncome = (grossIncome) => {
-        let taxCost = 0;
-        let ninoCost = 0;
+    const calculateTaxCost = (grossIncome, bracketArray, costsArray) => {
+        let runningCost = 0;
         let i = 1;
 
-        // Calculate net income from each bracket
-        while (i < taxBrackets.length) {
-            if (grossIncome <= taxBrackets[i]) {
-                const earnedInBracket = grossIncome - taxBrackets[i-1]
-                taxCost += calculateTax(earnedInBracket, taxAmounts[i-1]);
+        // Calculate total cost from each tax bracket
+        while (i < bracketArray.length) {
+            if (grossIncome <= bracketArray[i]) {
+                const earnedInBracket = grossIncome - bracketArray[i-1];
+                runningCost += calculateCost(earnedInBracket, costsArray[i-1]);
                 break;
             } else {
-                const bracketWidth = taxBrackets[i] - taxBrackets[i - 1];
-                taxCost += calculateTax(bracketWidth, taxAmounts[i-1]);
+                const bracketWidth = bracketArray[i] - bracketArray[i - 1];
+                runningCost += calculateCost(bracketWidth, costsArray[i-1]);
             }
             i++;
         }
 
-        // If income is greater than the highest bracket, add the difference
-        if (grossIncome > taxBrackets[i-1]) {
-            const earnedAtMax = grossIncome - taxBrackets[i-1]
-            taxCost += calculateTax(earnedAtMax, taxAmounts[i-1]);
+        // If income is greater than the highest tax bracket, add the difference
+        if (grossIncome > bracketArray[i-1]) {
+            const earnedAtMax = grossIncome - bracketArray[i-1];
+            runningCost += calculateCost(earnedAtMax, costsArray[i-1]);
         }
+
+        return parseFloat(runningCost.toFixed(2));
+    }
+
+    const calculateNetIncome = (grossIncome) => {
+        let taxCost = calculateTaxCost(grossIncome, taxBrackets, taxAmounts);
+        let ninoCost = calculateTaxCost(grossIncome, ninoBrackets, ninoAmounts);
 
         const totalCost = taxCost + ninoCost;
         return grossIncome - totalCost;
@@ -43,5 +49,4 @@ export function TaxCalcUtils() {
     return {
         calculateNetIncome,
     }
-
 }
