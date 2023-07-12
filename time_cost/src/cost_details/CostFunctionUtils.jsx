52ba44ff8cf73a57f590b2ec568e82c9868bea5e
JarrenWhite <phillrucci@hotmail.co.uk>
2023-07-12 22:24:09 +0100

    (#8) Taxed pay per hour is now automatically calculated.

--- time_cost/src/cost_details/CostFunctionUtils.jsx
+++ time_cost/src/cost_details/CostFunctionUtils.jsx
@@ -1,8 +1,12 @@
+import {TaxCalcUtils} from "../tax_calcs/TaxCalcUtils.jsx";
+
 export function CostFunctionUtils({salary, payFrequency, hours, hourFrequency}) {
     // Number of times per year that the event occurs
     const timesPerYear = {'Week': 52, 'Month': 12, 'Year': 1, 'Four Weeks': 13}
 
-    const recalculatePayPerHour = () => {
+    const {calculateNetIncome} = TaxCalcUtils();
+
+    const recalculatePayPerHour = (predictTax) => {
         // If payFrequency is hourly, already have pay per hour
         if (payFrequency === 'Hour') {
             return parseFloat(salary);
@@ -11,7 +15,16 @@ export function CostFunctionUtils({salary, payFrequency, hours, hourFrequency}) 
         // Else, calculate pay per year & hours per year
         const yearlySalary = parseFloat(salary) * timesPerYear[payFrequency];
         const yearlyHours = parseFloat(hours) * timesPerYear[hourFrequency];
-        return (yearlySalary / yearlyHours);
+        const normalPayPerHour = yearlySalary / yearlyHours;
+
+        // If tax to be predicted, return both types
+        if (predictTax) {
+            const taxedPayPerHour = calculateNetIncome(yearlySalary) / yearlyHours;
+            return [normalPayPerHour, taxedPayPerHour];
+        }
+
+        // Else just the one
+        return [normalPayPerHour];
     }
 
     return {
